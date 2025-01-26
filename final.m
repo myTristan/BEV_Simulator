2b63f60ec35ea6217b055af50a66f202dd8f5aea
Haimingyue Zhang <149091635+myTristan@users.noreply.github.com>
2025-01-27 07:31:52 +0800

    Add files via upload

---
+++ final.m
@@ -0,0 +1,15 @@
+load R_cha_SOC
+load R_dch_SOC
+load Vocv_cha_SOC
+load cycle
+
+timedata = [size(NEDC,1)
+size(CAFE,1)
+size(CNBUS,1)
+size(EUDC,1)
+size(FTP_75,1)
+size(HWFET,1)
+size(JC10,1)
+size(JC10_15,1)
+size(JC15,1)
+size(UDC,1)];
